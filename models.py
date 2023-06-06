389a5ea69be1919d98f2e8b73b461fca70e5963a
guanarp <jriosparquet@gmail.com>
2023-06-05 23:40:03 -0400

    modified models to add a pizza_ingredient_association table

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from enum import Enum, auto
 
 from pydantic import BaseModel
 
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Table
 from sqlalchemy.orm import relationship
 
 from .database import Base
@@ -21,6 +21,11 @@ class UserType(Enum):
     staff = 2
     SU = 3
 
+
+pizza_ingredient_association = Table(
+    'pizza_ingredient', Base.metadata,
+    Column('pizza_id', Integer, ForeignKey('pizzas.id')),
+    Column('ingredient_id',Integer, ForeignKey('ingredients.id')))
 
 #SQLAlchemy models
 
@@ -44,11 +49,12 @@ class Pizza(Base):
     is_active = Column(Boolean, default = False)
     
     ingredients = relationship(
-        "Ingredients", back_populates="active_pizzas")
-    ingredients_number = len(ingredients)
+        "Ingredient", secondary = pizza_ingredient_association, 
+        back_populates="pizzas")
+    #ingredients_number = len(ingredients)
 
-    def get_ingredients_number(self): #verificar si necesito la funcion
-        return len(self.ingredients)
+    #def get_ingredients_number(self): #verificar si necesito la funcion
+    #    return len(self.ingredients)
 
 
 class Ingredient(Base):
@@ -58,6 +64,7 @@ class Ingredient(Base):
     name = Column(String, index=True)
     category = Column(Enum(IngredientType), default=IngredientType.basic) #verificar si necesito index = true
 
-    active_pizzas = relationship(
-        "Pizza", back_populates = "ingredients")
+    pizzas = relationship(
+        "Pizza", secondary = pizza_ingredient_association, 
+        back_populates = "ingredients")
     
